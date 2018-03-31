5d925a73317505148aecf59f4b81500de3ff7439
Adam Bottchen <adam.bottchen@puppet.com>
2018-03-30 21:35:22 -0700

    turned players into a list, adding css

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,27 +1,50 @@
 import React, { Component } from 'react';
+import './PlayerList.css'
 
 class PlayerList extends Component {
   constructor(props) {
     super(props);
     /* Binds 'this' to the component state rather than the local variable scope */
-    this.handleChange = this.handleChange.bind(this);
+    this.onPlayerSelect = this.onPlayerSelect.bind(this);
   }
 
-  handleChange(event) {
-    const onBlock = event.target.value
+  onPlayerSelect(player) {
     this.props.updateGlobalState({
-      onBlock: onBlock 
+      onBlock: player 
     });
   }
 
   render() {
+    const playerList = [
+      'Brown, Antonio',
+      'Allen, Keenan',
+      'Hill, Tyreek',
+      'Hunt, Kareem',
+      'Mahomes, Patrick',
+      'Goff, Jared',
+      'McCoy, LeSean',
+      'Jones, Julio',
+      'Olsen, Greg',
+      'Butker, Harrison',
+      'Shepard, Sterling',
+      'McKinnon, Jerick',
+      'Henry, Derrick',
+      'Doyle, Jack',
+      'Williams, Jamaal',
+      'Lewis, Dion'
+    ];
     return (
       <div className="panel">
-        <h3>Update Block</h3>
-        <textarea
-          style={{width: "100%", hieght: "100%"}}
-          onChange={this.handleChange}
-        />
+        <h3>Players</h3>
+        <div className="player-list-div">
+          <ul className="player-list">
+              {
+                playerList.map((player,index) => {
+                  return <li className="player" key={ index } onClick={ this.onPlayerSelect.bind(this, player) } >{ player }</li>;
+                })
+              }
+          </ul>
+        </div>
       </div>
     );
   }
